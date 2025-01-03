11fa3274e2ecb1a49fcd31295c137004c4134dcc
Riya Gupta <riyagupta3825@gmail.com>
2025-01-03 11:05:05 +0530

    added loop to ask mulitple questions

--- transcript.py
+++ transcript.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 video_id = '5zuF4Ys1eAw&t'
 transcipt = YouTubeTranscriptApi.get_transcript(video_id)
-# refer to notes(id=1)
+# refer to notes(id1)
 transcript_text = ' '.join([entry['text'] for entry in transcipt])
 # print(transcipt_text)
 
@@ -16,15 +16,19 @@ def ans_ques(transcipt, question):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         store=True,
+        # refer to notes(id3)
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": f"Based on the following transcript, answer the question:\n\nTranscript:\n{transcript_text}\n\nQuestion: {question}"}
-        ]
+        ],
+        # refer to notes(id2)
+        max_tokens=50
     )
 
     return completion.choices[0].message.content
 
-
-question = input("Enter your query: ")
-answer = ans_ques(transcript_text, question)
-print(answer)
+n = int(input("Enter number of questions you want to ask: "))
+for i in range(n):
+    question = input("Enter your query: ")
+    answer = ans_ques(transcript_text, question)
+    print(answer)
